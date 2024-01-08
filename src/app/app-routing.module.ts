f483b952ee91a6d7ae2e08b02c4da8e4a953c55a
ShiraNarinsky <shiran79999@gmail.com>
2024-01-08 14:03:36 +0200

    add login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AuthGuard } from './auth.guard';
-// import { RequestComponent } from './components/request/request.component';
-// import { MenuComponent } from './components/menu/menu.component';
-// import { VolunteerDetailsComponent } from './components/volunteer-details/volunteer-details.component';
-// import { FeedbackHistoriesComponent } from './components/feedback-histories/feedback-histories.component';
-// import { DistrictService } from './services/district.service';
-// import { CategoryService } from './services/category.service';
 
 const routes: Routes = [
   { path: '', component: SignUpComponent, canActivate: [AuthGuard] },
